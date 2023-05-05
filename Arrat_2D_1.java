0e4d4985233c2bd467ba163ec5e62fbb715de4e0
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-05 18:17:28 +0000

    Fri, May 5, 2023, 11:47 PM +05:30

--- Arrat_2D_1.java
+++ Arrat_2D_1.java
@@ -1,6 +1,18 @@
 public class Array_2D_1{
     public static void main(String[] args){
-        int[][] integers = {{1,2,3},{4,5},{}}
-        sum_rows()
+        int[][] integers = {{1,2,3},{4,5},{6,7,8,9}};
+        sum_rows(integers);
+        sum_columns(integers);
+    }
+
+    private static void sum_rows(int[][] integers){
+        for(int i = 0; i<integers.length; i++){
+            int sum = 0;
+            for(int j = 0;j<integers[i].length; j++){
+                sum += integers[i][j];
+            }
+
+            System.out.println("");
+        }
     }
 }
