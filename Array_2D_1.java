1f958492c1d0460cacea94ddd125c94cfb6b8bb7
Paulie-Aditya <122125787+Paulie-Aditya@users.noreply.github.com>
2023-05-05 19:07:15 +0000

    Sat, May 6, 2023, 12:37 AM +05:30

--- Array_2D_1.java
+++ Array_2D_1.java
@@ -17,12 +17,14 @@ public class Array_2D_1{
     }
 
     private static void sum_columns(int[][] integers){
-        int max_length = 0;
+        /*int max_length = 0;
         for(int x = 0;x<integers.length;x++){
             if max_length<integers[x]
             max_length = integers[x];
-        }
-        
+        }*/
+
+        //int[][] integers = {{1,2,3},{4,5},{6,7,8,9}};
+
         for(int i = 0; i<integers[i].length;i++){
             int sum = 0;
             for(int j = 0; j<integers.length;j++){
